31db31547b07a97063b9dc492650a8b0e00de4cb
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2024-11-04 11:53:58 +0100

    population data prepared

--- 3-worldpop-data.R
+++ 3-worldpop-data.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(readxl)
+library(sjPlot)
+library(sjmisc)
 
 world_bank_pop %>% summary() # for codes? 
 
@@ -23,3 +25,20 @@ d[19, 1] <- "United States of America"
 p_21_usa <- p_country_2023 %>% filter(`Region, subregion, country or area *` %in% d$country) # 21 :) 
 
 # TODO rowsums and lefjoin 
+
+p_21_usa <- p_21_usa %>% mutate(across(.cols = `0`:`100+`, .fns = as.numeric))
+
+str(p_21_usa)
+
+p_21_usa <- sjmisc::row_sums(x = p_21_usa, `0`:`100+`, n = Inf)
+
+p_join <- p_21_usa %>%
+  rename(country = `Region, subregion, country or area *`, 
+         population = rowsums) %>% 
+  dplyr::select(country, population)
+
+d <- left_join(d, p_join)
+
+# population is in K, make it in 1: 
+
+d <- d %>% mutate(population = population * 1000)
