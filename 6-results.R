6d80be41e2189a6151fe89af784c3d29868ce98e
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2025-06-30 10:50:41 +0200

    moved to new markdown

--- 6-results.R
+++ 6-results.R
@@ -124,31 +124,4 @@ sjPlot::tab_model(m4_control, m5_population, m7_arobust)
 
 # tabela semi-partial correlations 
 
-# rozrzut IND vs. GPTUSE z etykietami krajów (na koniec wyników)
 
-point_ind_gptuse <-
-  ggplot(df) +
-  geom_point(aes(x = IND, y = GPTUSE), size = 3) +
-  geom_label_repel(
-    aes(label = country, x = IND, y = GPTUSE),
-    min.segment.length = 0.5,
-    force = 1,
-    box.padding = 0.6,
-    segment.size = 0.2,
-    size = 2,
-    max.overlaps = Inf,
-    fontface = "bold",
-    alpha = 0.8
-  ) +
-  coord_fixed() +
-  scale_x_continuous(breaks = c(10, 20, 30, 40, 50, 60, 70, 80)) +
-  scale_y_continuous(breaks = c(10, 20, 30, 40, 50, 60, 70, 80),
-                     expand = expansion(add = 20)) +
-  theme_classic() +
-  theme(axis.text = element_text(size = 6),
-        axis.title = element_text(size = 8))
-
-print(point_ind_gptuse)
-
-# ggsave("point_ind_gptuse.png", plot = point_ind_gptuse, units = "cm",
-#        width = 12, height = 12) # 12 wygląda dobrze, poprawić nazwy osi
